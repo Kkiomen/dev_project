5ca900b15f8c234ecba40321a29a684f7b3dfb90
Jakub Owsianka <j.owsianka@wiseb2b.eu>
2026-02-05 20:25:57 +0100

    Fix

--- app/Http/Resources/RowResource.php
+++ app/Http/Resources/RowResource.php
@@ -10,15 +10,27 @@ class RowResource extends JsonResource
     public function toArray(Request $request): array
     {
         $values = [];
+        $fields = [];
 
         if ($this->relationLoaded('cells')) {
             $cells = $this->cells->keyBy('field_id');
             // Always sort fields by position to ensure consistent order
-            $fields = $this->table->fields->sortBy('position');
+            $tableFields = $this->table->fields->sortBy('position');
 
-            foreach ($fields as $field) {
+            foreach ($tableFields as $field) {
                 $cell = $cells->get($field->id);
-                $values[$field->public_id] = $cell?->getValue() ?? $field->getDefaultValue();
+                $value = $cell?->getValue() ?? $field->getDefaultValue();
+                $values[$field->public_id] = $value;
+
+                // Add field info with camelCase name key for easier access
+                $nameKey = \Illuminate\Support\Str::camel($field->name);
+                $fields[$nameKey] = [
+                    'field_id' => $field->public_id,
+                    'name' => $field->name,
+                    'type' => $field->type->value,
+                    'value' => $value,
+                    'cell_id' => $cell?->id,
+                ];
             }
         }
 
@@ -27,6 +39,7 @@ class RowResource extends JsonResource
             'table_id' => $this->table->public_id,
             'position' => $this->position,
             'values' => $values,
+            'fields' => $fields,
             'cells' => CellResource::collection($this->whenLoaded('cells')),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
